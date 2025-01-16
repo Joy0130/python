42471a778b2bfb16869cec9962eee7080dbb27af
joy <gm10290014@gmail.com>
2025-01-16 16:40:11 +0800

    Update_yaml&build-docker

--- build-docker.sh
+++ build-docker.sh
@@ -4,5 +4,5 @@ pip freeze > requirements.txt
 pip uninstall -r requirements.txt -y
 deactivate
 rm -r venv/
-#sudo docker build -t "nas-tvm.futsu.com.tw:9999/python-test-container:latest" .
-sudo docker run -d -p 5000:5002 --name python-test-container ghcr.io/joy0130/python:latest
+sudo docker build -t "nas-tvm.futsu.com.tw:9999/python-test-container:latest" .
+#sudo docker run -d -p 5000:5002 --name python-test-container ghcr.io/joy0130/python:latest
